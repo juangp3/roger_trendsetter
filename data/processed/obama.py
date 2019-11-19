c6def943e07418f35d23288be915189e53c1dbc3
juangp3 <juangp3@gmail.com>
2019-11-19 23:09:27 +0100

    Create video robot

---
+++ data/processed/obama.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+"""
+@File    : obama.py.py
+@Date    : 11/19/19 11:03 PM
+@Project : roger_trendsetter
+@Author  : juangp3
+"""
+content = [{
+    'text': 'Barack Hussein Obama II ( (listen); born August 4, 1961) is an American attorney and politician who served as the 44th president of the United States from 2009 to 2017.',
+    'keywords': ['Barack Hussein Obama II', 'American attorney', 'politician',
+                 '44th president of the United States', 'listen', 'August']}, {
+    'text': 'A member of the Democratic Party, he was the first African American to be elected to the presidency.',
+    'keywords': ['member of the Democratic Party', 'presidency']}, {
+    'text': 'He previously served as a U.S. senator from Illinois from 2005 to 2008 and an Illinois state senator from 1997 to 2004.Obama was born in Honolulu, Hawaii.',
+    'keywords': ['U.S. senator', 'Illinois state senator', 'Illinois', 'Honolulu', 'Hawaii',
+                 '2004.Obama']}, {
+    'text': 'After graduating from Columbia University in 1983, he worked as a community organizer in Chicago.',
+    'keywords': ['Columbia University', 'community organizer', 'Chicago']}, {
+    'text': 'In 1988, he enrolled in Harvard Law School, where he was the first black president of the Harvard Law Review.',
+    'keywords': ['Harvard Law School', 'first black president of the Harvard Law Review']}, {
+    'text': 'After graduating, he became a civil rights attorney and an academic, teaching constitutional law at the University of Chicago Law School from 1992 to 2004.',
+    'keywords': ['civil rights attorney', 'constitutional law', 'University of Chicago Law School']}, {
+    'text': 'He represented the 13th district for three terms in the Illinois Senate from 1997 until 2004, when he ran for the U.S. Senate.',
+    'keywords': ['Illinois Senate', '13th district', 'terms', 'U.S. Senate']}, {
+    'text': 'He received national attention in 2004 with his March primary win, his well-received July Democratic National Convention keynote address, and his landslide November election to the Senate.',
+    'keywords': ['national attention', 'March primary win', 'Senate', 'landslide November election',
+                 'July Democratic National Convention keynote address']}, {
+    'text': 'In 2008, he was nominated for president a year after his campaign began, after a close primary campaign against Hillary Clinton.',
+    'keywords': ['close primary campaign', 'Hillary Clinton', 'president', 'campaign']},
+    {'text': 'He was elected over Republican John McCain and was inaugurated on January 20, 2009.',
+     'keywords': ['Republican John McCain', 'January']}]
